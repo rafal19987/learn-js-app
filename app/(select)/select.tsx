3dfc3a8e5ba4a9906245e9cb8c9a6285c81f7b88
Rafixx <rafal987.1@o2.pl>
2023-11-02 20:42:22 +0100

    feat: create select subject route page

--- app/(select)/select.tsx
+++ app/(select)/select.tsx
@@ -1,11 +1,46 @@
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { SubjectList } from '../../src/components/SubjectList';
 
-const SelectPage = () => {
+export default function SelectPage() {
   return (
-    <View>
-      <Text>This is Quiz route</Text>
+    <View style={styles.container}>
+      <View style={styles.headlineContainer}>
+        <Text style={styles.headline}>Select subject</Text>
+      </View>
+      <View style={styles.section}>
+        <Text style={styles.sectionDescription}>Avaiable subjects</Text>
+        <SubjectList />
+      </View>
     </View>
   );
-};
+}
 
-export default SelectPage;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    gap: 64,
+  },
+  headlineContainer: {
+    width: '100%',
+    paddingVertical: 12,
+  },
+  headline: {
+    color: 'white',
+    fontFamily: 'Roboto',
+    fontSize: 28,
+    fontWeight: '700',
+    textAlign: 'center',
+  },
+  section: {
+    position: 'relative',
+    width: '100%',
+    padding: 24,
+  },
+  sectionDescription: {
+    marginBottom: 24,
+    color: 'white',
+    fontSize: 18,
+    fontWeight: '600',
+  },
+});
