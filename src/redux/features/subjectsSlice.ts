b02a197423505e40f08d170cb2d39f5a86760bdb
Rafixx <rafal987.1@o2.pl>
2023-11-10 21:27:28 +0100

    fix: change type for id

--- src/redux/features/subjectsSlice.ts
+++ src/redux/features/subjectsSlice.ts
@@ -2,21 +2,21 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { type RootState } from '../store';
 
 export type TSubjects = {
-  id: number;
+  id: string;
   title: string;
 };
 
 const initialState: TSubjects[] = [
   {
-    id: 1,
+    id: '1',
     title: 'Core concepts',
   },
   {
-    id: 2,
+    id: '2',
     title: 'JS Arrays Methods',
   },
   {
-    id: 3,
+    id: '3',
     title: 'yemen',
   },
 ];
